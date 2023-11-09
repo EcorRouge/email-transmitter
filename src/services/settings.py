484888de871570b93a967f9429cdb2bcac10695e
afeef <janjua.afeef@gmail.com>
2023-11-10 00:43:56 +0500

    read config.json in settings

--- src/services/settings.py
+++ src/services/settings.py
@@ -1,14 +1,28 @@
+import json
+from typing import Dict
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    SERVICE_NAME: str
     SOURCE_EMAIL: str
     ERROR_REPORTING_EMAIL: str
     CONFIG_JSON_PATH: str
 
-    @staticmethod
-    def get_event(event_name: str):
-        return event_name
+    _config: Dict
+
+    def read_config_json(self):
+        with open(self.CONFIG_JSON_PATH) as config:
+            self._config = json.load(config)
+
+    def get_config(self):
+        for configuration in self._config['configurations']:
+            if configuration['provider'] == self.SERVICE_NAME:
+                return configuration
+
+    def get_event(self, event_name: str):
+        return self._config['events'][event_name]
 
 
 class MailjetSettings(Settings):
