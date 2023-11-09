c501ee975222126c4c402fb193f0ce3d00644b17
afeef <janjua.afeef@gmail.com>
2023-11-10 00:12:15 +0500

    register service and settings in factory

--- src/factories.py
+++ src/factories.py
@@ -1,27 +1,28 @@
-from typing import Type, Dict
+from typing import Type, Dict, Tuple
 
-from services import EmailService, MailjetService, SESService
+from services import EmailService, MailjetService, SESService, MailjetSettings, SESSettings, Settings
 
 
 class EmailServiceFactory:
     def __init__(self):
-        self._services: Dict[str, EmailService] = {}
+        self._services: Dict[str, Tuple[Type[EmailService], Type[Settings]]] = {}
 
-    def register_service(self, key, service: EmailService):
-        self._services[key] = service
+    def register_service(self, key, service: Type[EmailService], settings: Type[Settings]):
+        self._services[key] = (service, settings)
 
     def create(self, key, **kwargs):
-        provider = self._services.get(key)
+        service_class, settings_class = self._services.get(key)
 
-        if not provider:
+        if not service_class:
             raise ValueError(key)
 
-        return provider(**kwargs)
+        settings = settings_class(**kwargs)
+        return service_class(settings=settings)
 
     def get(self, key, **kwargs):
         return self.create(key, **kwargs)
 
 
 email_factory = EmailServiceFactory()
-email_factory.register_service('mailjet', MailjetService())
-email_factory.register_service('ses', SESService())
+email_factory.register_service('mailjet', MailjetService, MailjetSettings)
+email_factory.register_service('ses', SESService, SESSettings)
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,3 +1,4 @@
 from .base import EmailService
 from .mailjet import MailjetService
 from .ses import SESService
+from .settings import Settings, MailjetSettings, SESSettings, EmailSettings
