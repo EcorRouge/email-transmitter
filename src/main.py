2ff39bdf799c54978417fa6d7bd02bd0c0ea709c
afeef <janjua.afeef@gmail.com>
2023-11-10 00:15:54 +0500

    testing email factory

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from factories import email_factory
+
 if __name__ == "__main__":
-    print('Hello world')
+    settings = {
+        'MAILJET_API_KEY': 'fake_api_key',
+        'MAILJET_API_SECRET': 'fake_api_secret'
+    }
+
+    mailjet_service = email_factory.get('mailjet', **settings)
